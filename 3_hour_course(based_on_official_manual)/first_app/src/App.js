b64d8a643d223ab78baec383d077a58bbad3fec9
briarey <egorsasim@mail.ru>
2022-06-21 08:23:23 -0700

    first function el

--- 3_hour_course(based_on_official_manual)/first_app/src/App.js
+++ 3_hour_course(based_on_official_manual)/first_app/src/App.js
@@ -1,32 +1,16 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
-    const [likes, setLikes] = useState(0);
+  
     const [value, setValue] = useState('Text in the input');
 
-    function increment() {
-        setLikes(likes + 1);
-    }
-
-    function decrement() {
-        setLikes(likes - 1);
-    }
 
   return (
     <div className="App">
-        <h1>{likes}</h1>
+        <Counter/>
         <h1>{value}</h1>
-
-        <input 
-            type="text" 
-            value={value}
-            onChange = {event => {
-                setValue(event.target.value);
-            }}>
-        </input>
-        <br></br>
-        <button onClick={ increment}>Increment</button>
-        <button onClick={ decrement}>Decrement</button>
+        <input onChange={(event) => {setValue(event.target.value)}} type={"text"} value={value}></input>
     </div>
   );
 }
