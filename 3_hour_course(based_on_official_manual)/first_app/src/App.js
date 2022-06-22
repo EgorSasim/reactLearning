4be4243a898fef3c8e88a269b751c6652292aff8
briarey <egorsasim@mail.ru>
2022-06-22 11:38:18 -0700

    working with lists

--- 3_hour_course(based_on_official_manual)/first_app/src/App.js
+++ 3_hour_course(based_on_official_manual)/first_app/src/App.js
@@ -1,26 +1,52 @@
 import React, { useState } from "react";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
 import './styles/App.css';
 
+
 function App() {
-  
-    const [value, setValue] = useState('Text in the input');
-
-
-  return (
-    <div className="App">
-        <PostItem post={{
+    
+    const [posts, setPosts] = useState([
+        {
             id: 1, 
             title: "Javascript",
             content: "Javascript is a programming language",
-        }}/>
-
-        <PostItem post={{
+        },
+        {
             id: 2, 
             title: "HTML",
             content: "HTML is a hypertext markup language",
-        }}/>
-      
+        },
+        {
+            id: 3, 
+            title: "DB",
+            content: "It's a data base",
+        },        
+    ])
+
+    const [posts2, setPosts2] = useState([
+        {
+            id: 1, 
+            title: "NAT",
+            content: "Network address translation: convert 'grey' IP into 'white' IP via cpecial stuff ",
+        },
+        {
+            id: 2, 
+            title: "HTTP",
+            content: "HTTP - hypertext transfer protocol",
+        },
+        {
+            id: 3, 
+            title: "Linux",
+            content: "Best operation system for free, but we use pirate Windows)",
+        },
+    ])
+
+    return (
+    <div className="App">
+        <PostList posts={posts} title={"Posts List 1"}/>
+        <PostList posts={posts2} title={"Posts List 2"}/>
     </div>
   );
 }
