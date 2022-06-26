b3a1e31350861736d08042f8f3a04b05d27e2dbe
briarey <egorsasim@mail.ru>
2022-06-26 05:16:57 -0700

    try to create searching component, but stucked, can't solve it correctly

--- 3_hour_course(based_on_official_manual)/first_app/src/App.js
+++ 3_hour_course(based_on_official_manual)/first_app/src/App.js
@@ -1,6 +1,7 @@
-import React, {useState } from "react";
+import React, {useMemo, useState } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyInput from "./components/UI/inout/MyInput";
 import MySelect from "./components/UI/select/MySelect";
 
 import './styles/App.css';
@@ -27,6 +28,20 @@ function App() {
     ])
 
     const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortedPosts = useMemo(() => {
+        console.log("Call getSortedPosts"); 
+        if (selectedSort)
+            return [...posts.sort()].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]));
+        return [posts];
+    }, [selectedSort, posts]);
+
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.includes(searchQuery))
+    }, [searchQuery, sortedPosts])
+    
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -39,14 +54,19 @@ function App() {
     const sortPosts = (sort) => {
         setSelectedSort(sort);
         console.log(sort);
-        setPosts( [...posts.sort()].sort((a,b) => a[sort].localeCompare(b[sort])))
     }
    
     return (
     <div className="App">
         <PostForm create={createPost} />
         <hr style={{margin: "5px"}}/>
-        
+            <MyInput 
+                type="text" 
+                placeholder="Search"
+                value={searchQuery}
+                onChange={ e => setSearchQuery(e.target.value)}
+            ></MyInput>
+
             <MySelect
                 value={selectedSort}
                 onChange={sortPosts}
@@ -58,7 +78,7 @@ function App() {
         />
         
         {posts.length !== 0
-            ? <PostList remove={removePost} posts={posts} title={"Posts List 1"}/>
+            ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Posts List 1"}/>
             : <h1 style={{textAlign: "center"}}>Posts not found</h1>
         } 
         
